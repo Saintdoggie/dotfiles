6d620da4dc939705d5e2bcc3fadda564eea1a7d8
Liam St. Ores <l@nklssjx.xyz>
2023-02-10 18:14:31 -0600

    include new files

---
+++ installer/lib/options.py
@@ -0,0 +1,40 @@
+import sys
+from lib.exec import execution
+from termcolor import colored
+
+
+class options:
+    def handleCommandLineOptions():
+        try:
+            if (sys.argv[1] == "update"):
+                        
+                # system installation
+                execution.install.system()
+
+                # home installation
+                execution.install.home()
+
+                # config installation
+                execution.install.configs()
+
+                #cleans the system after update
+                execution.install.clean()
+
+
+                #warning message
+                print(colored("Notice: ", "yellow"),"Some log files are stored in installer/output, please look through them for issues.")
+
+            if (sys.argv[1] == "config"):
+
+                execution.install.configs()
+
+            if (sys.argv[1] == "clean"):
+                execution.install.clean()
+
+        except:
+            print("Usage:")
+            print(" --update  Updates the system")
+            print(" --config  Updates the configs")
+            print(" --clean   cleans system")
+
+
